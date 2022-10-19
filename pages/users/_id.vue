ada2527eec96e8bf60c5dc02bcee0415e3fda9f9
Maxim Misyuchenko <m.misyuchenko@tg.dunice.net>
2022-10-19 17:57:40 +0300

    initial commit

---
+++ pages/users/_id.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>{{ id }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    id() {
+      return this.$route.params.id;
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
